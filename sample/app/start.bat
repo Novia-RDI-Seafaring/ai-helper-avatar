12922d7714da2ff98026bfb24e3ce31f4ad367fe
DBengs <89458598+DBengs@users.noreply.github.com>
2024-03-01 22:09:03 +0200

    Uploaded sample app.

---
+++ sample/app/start.bat
@@ -0,0 +1,7 @@
+@echo off
+
+call conda activate test_environment
+
+python main.py
+
+pause
---
+++ sample/app/virtual_environments/create_test_environment.bat
@@ -0,0 +1,21 @@
+:: Prerequisites:
+::   Miniconda/Anaconda
+
+:: Set environmental variables
+set ENVIRONMENT_NAME=test_environment
+
+:: Remove old conda environment (if it exists) and create a new environment
+call conda deactivate
+call conda env remove -n %ENVIRONMENT_NAME%
+call conda create -n %ENVIRONMENT_NAME% python=3.11 -y
+
+:: Activate conda environment
+call conda activate %ENVIRONMENT_NAME%
+
+:: Install dependencies
+call pip install flask flask-cors flask-socketio keyboard requests termcolor
+
+:: Deactivate conda environment
+call conda deactivate
+
+pause
