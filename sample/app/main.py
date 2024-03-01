88d742612f53bd7b23fb29f30200f5d83983e69c
DBengs <89458598+DBengs@users.noreply.github.com>
2024-03-01 22:09:03 +0200

    Uploaded sample app.

---
+++ sample/app/main.py
@@ -0,0 +1,56 @@
+import configparser
+import keyboard
+import os
+import sys
+import time
+
+from types import SimpleNamespace
+
+from webserver import WebServer
+
+# Check Python version to ensure compatibility
+class UnsupportedVersion(Exception):
+    pass
+
+MIN_VERSION, VERSION_LESS_THAN = (3, 5), (4, 0)
+if sys.version_info < MIN_VERSION or sys.version_info >= VERSION_LESS_THAN:
+    raise UnsupportedVersion('requires Python %s,<%s' % ('.'.join(map(str, MIN_VERSION)), '.'.join(map(str, VERSION_LESS_THAN))))
+
+# Initialize a simple namespace object to hold the application context
+context = SimpleNamespace()
+
+# Define common paths
+context.script_directory = os.path.dirname(os.path.abspath(__file__))
+context.static_directory = os.path.join(context.script_directory, 'static')
+
+# Load configuration from 'config.ini'
+config_path = os.path.join(context.script_directory, 'config.ini')
+
+context.config = configparser.ConfigParser()
+context.config.read(config_path)
+
+# Create the webserver
+context.webserver = WebServer(context)
+
+# Start the web server and open the default web page
+context.webserver.start_server()
+context.webserver.open_page()
+
+print('Press ESC to stop')
+
+# Main loop to keep the application running
+try:
+    while True:
+        if keyboard.is_pressed('esc'):
+            break
+
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    pass
+
+# Cleanup and shutdown procedures
+context.webserver.close()
+
+time.sleep(1)
+
+print('Program stopped')
---
+++ sample/app/webserver.py
@@ -0,0 +1,119 @@
+import logging
+import requests
+import secrets
+import threading
+import webbrowser
+
+from flask import Flask, request, abort
+from flask_cors import CORS
+from flask_socketio import SocketIO, emit
+from termcolor import cprint
+
+PRINT_COLOR = 'light_blue'
+
+class WebServer:
+    '''A web server class for serving static files and handling WebSocket connections.'''
+
+    def __init__(self, context):
+        self._context = context
+
+        self._server_thread = None
+
+        self._port = self._context.config.getint('webserver', 'port')
+
+        # Setup the Flask app with static file serving
+        self._server = Flask(
+            context.config.get('webserver', 'name'),
+            static_url_path='/',
+            static_folder=context.static_directory
+        )
+
+        # Enable CORS for the server to accept requests from different origins
+        CORS(self._server)
+
+        # Secure the Flask session with a random secret key
+        self._server.config['SECRET_KEY'] = secrets.token_hex(16)
+
+        # Only log errors to console
+        log = logging.getLogger('werkzeug')
+        log.setLevel(logging.ERROR)
+
+        # Setup SocketIO using 'threading' for asynchronous handling
+        self._socketio = SocketIO(self._server, async_mode='threading', cors_allowed_origins='*')
+
+        # Multithreading event flags for signaling when a reply has been received
+        self._test_reply_event = threading.Event()
+
+        # HTTP route handlers
+
+        @self._server.route('/')
+        def handle_index():
+            return self._server.send_static_file('index.html')
+
+        @self._server.route('/shutdown')
+        def handle_shutdown():
+            if request.remote_addr == '127.0.0.1':
+                self._socketio.stop()
+                return 'OK', 200
+            else:
+                abort(404)
+
+        # Socket.IO handlers
+
+        @self._socketio.on('connect')
+        def handle_connect():
+            cprint(f'WebServer: Socket.IO client connected ({request.sid})', PRINT_COLOR)
+
+        @self._socketio.on('disconnect')
+        def handle_disconnect():
+            cprint(f'WebServer: Socket.IO client disconnected ({request.sid})', PRINT_COLOR)
+
+        @self._socketio.on('test_reply')
+        def handle_test_reply(data=None):
+            cprint('WebServer: Got message on "test_reply" namespace')
+
+            self._test_reply_event.set()
+
+    def send_message_test(self):
+        '''Send 'test' to JavaScript via Socket.IO.'''
+
+        self._test_reply_event.clear()
+
+        with self._server.app_context():
+            self._socketio.emit('test', { 'data': 'some_data' })
+
+    def wait_for_test_reply(self, timeout=None):
+        '''Wait for a reply on the 'test_reply' namespace.'''
+
+        if self._test_reply_event.is_set():
+            return
+        self._test_reply_event.wait(timeout)
+
+    def start_server(self):
+        '''Starts the web server on a separate thread for non-blocking operations.'''
+
+        self._server_thread = threading.Thread(target=self._run, daemon=True)
+        self._server_thread.start()
+
+        cprint(f'WebServer: Webserver started at URL http://127.0.0.1:{self._port}', PRINT_COLOR)
+
+    def _run(self):
+        '''The internal method to run the Flask app.'''
+
+        self._socketio.run(self._server, host='127.0.0.1', port=self._port)
+
+    def stop_server(self):
+        '''Stop the web server by making a shutdown request via HTTP.'''
+
+        requests.get(f'http://127.0.0.1:{self._port}/shutdown')
+
+        self._server_thread.join()
+
+        cprint('WebServer: Webserver stopped', PRINT_COLOR)
+
+    def open_page(self):
+        '''Open the web server's main page in the default web browser.'''
+
+        cprint('WebServer: Opening web page...', PRINT_COLOR)
+
+        webbrowser.open_new(f'http://127.0.0.1:{self._port}')
