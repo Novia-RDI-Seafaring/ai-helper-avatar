768ecf48f862ebabfe55a3633f3f78332639de78
DBengs <89458598+DBengs@users.noreply.github.com>
2024-03-22 14:00:21 +0200

    Update create_test_environment.bat

--- sample/app/virtual_environments/create_test_environment.bat
+++ sample/app/virtual_environments/create_test_environment.bat
@@ -13,7 +13,7 @@ call conda create -n %ENVIRONMENT_NAME% python=3.11 -y
 call conda activate %ENVIRONMENT_NAME%
 
 :: Install dependencies
-call pip install flask flask-cors flask-socketio requests termcolor
+call pip install flask flask-cors requests termcolor
 
 :: Deactivate conda environment
 call conda deactivate
