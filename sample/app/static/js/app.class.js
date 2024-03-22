cc00ff9869d40dec7ab8f314fb929732eb5ecb12
JussiHaap <118436666+JussiHaap@users.noreply.github.com>
2024-03-22 14:43:34 +0200

    changed the rotation of the camera & objects to match the mockup

--- sample/app/static/js/app.class.js
+++ sample/app/static/js/app.class.js
@@ -105,8 +105,8 @@ export default class App {
 
         // Create a typical perspective camera
         this.#context.camera = new THREE.PerspectiveCamera(75, 1, 0.1, 10);
-        this.#context.camera.position.set(-.35, 1.4, 1.15)
-        this.#context.camera.rotation.set(0, .25, 0);  
+        this.#context.camera.position.set(-.45, 1.6, 1.15)
+        this.#context.camera.rotation.set(-.10, 0, 0);  
 
         // Create fly controls for easier debugging
         this.#flyControls = new FlyControls(this.#context.camera, renderer.domElement);
