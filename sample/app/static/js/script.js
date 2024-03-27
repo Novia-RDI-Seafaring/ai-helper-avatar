368632d7f9cff7f1196f6167267fe0f7a45349e8
Lamin Jatta <lamin.jatta@novia.fi>
2024-03-27 13:45:22 +0200

    query loading time and fix chatbot to a height for scroll

--- sample/app/static/js/script.js
+++ sample/app/static/js/script.js
@@ -2,19 +2,24 @@ document.addEventListener('DOMContentLoaded', function() {
     var sendButton = document.getElementById('send');
     var inputField = document.querySelector('input[name="ai"]');
     var chatUI = document.querySelector('.chat');
-    var loadingIndicator = document.getElementById('loadingIndicator');
+    var loadingIndicator = document.getElementById('loadingState');
+    let chat = document.querySelector('.chatUI');
 
+
+
+    // Send the user's query to the server when the user clicks the send button
     sendButton.addEventListener('click', function(event) {
         event.preventDefault(); // Prevent the form from submitting normally
 
         var userInput = inputField.value;
         if (userInput.trim() === '') return; // Do not send empty queries
 
-        // Show loading indicator
-        loadingIndicator.style.display = 'block';
-
         // Display the user's query in the chat
         displayMessage(userInput, 'outgoing');
+
+        // Show loading indicator
+        loadingIndicator.style.display = 'block'; // TODO: Show the loading indicator (current one not necessary, but it's a good practice to show the user that something is happening in the background)
+
 
         // Send the request to the server
         var url = '/ask?query=' + encodeURIComponent(userInput);
@@ -35,9 +40,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function displayMessage(message, type) {
         var messageElement = document.createElement('div');
-        messageElement.className = 'message ' + type; // Different classes for styling incoming/outgoing messages
+        messageElement.className = 'message ' + type; // I apply different classes (first and second child) for styling incoming/outgoing messages
         messageElement.innerHTML = `<p class="messageText">${message}</p><p class="messageTime">${new Date().toLocaleTimeString()}</p>`;
         chatUI.appendChild(messageElement);
+        
+        chat.scrollTop = chat.scrollHeight; // Scroll to the bottom of the chat window
+
     }
 
     function displayMessageHistory(messageHistory) {
@@ -48,5 +56,8 @@ document.addEventListener('DOMContentLoaded', function() {
         messageElement.className = 'message ' + type;
         messageElement.innerHTML = `<p class="messageText">${lastMessage.message}</p><p class="messageTime">${new Date().toLocaleTimeString()}</p>`;
         chatUI.appendChild(messageElement);
+
+        // Scroll to the bottom of the chat window
+        chat.scrollTop = chat.scrollHeight;
     }
 });
