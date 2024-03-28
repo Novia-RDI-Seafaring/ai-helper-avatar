b91f5f1215f6637c0097f87f39754ac298fbe839
DBengs <89458598+DBengs@users.noreply.github.com>
2024-03-28 12:32:32 +0200

    Moved searchable_pdf init.

--- sample/app/webserver.py
+++ sample/app/webserver.py
@@ -4,7 +4,6 @@ import secrets
 import threading
 import webbrowser
 import os
-import json
 
 from flask import Flask, request, abort, jsonify
 from flask_cors import CORS
@@ -19,23 +18,6 @@ class WebServer:
 
     def __init__(self, context):
         self._context = context
-
-        print(str(os.path.join(os.path.dirname(__file__), 'static/demo_data/he-specification.pdf')))
-
-        with open(str(os.path.join(os.path.dirname(__file__), 'static/demo_data/he-specification_schema.json'))) as user_file:
-            file_contents = user_file.read()
-        json_value_string = json.dumps(json.loads(file_contents))
-        
-        with open(str(os.path.join(os.path.dirname(__file__), 'static/demo_data/he-specification.json'))) as json_schema_file:
-            json_schema_contents = json_schema_file.read()            
-            json_schema_string = json.dumps(json.loads(json_schema_contents))
-
-        # Create searchable PDF instance
-        self._searchable_pdf = SearchablePDF(
-            str(os.path.join(os.path.dirname(__file__), 'static/demo_data/he-specification.pdf')),
-            json_value_string,
-            json_schema_string
-        )
 
         self._server_thread = None
 
@@ -66,9 +48,11 @@ class WebServer:
 
         @self._server.route('/ask', methods=['GET'])
         def handle_message():
-            query = request.args.get('query', '') # Default to empty string if not provided
-            print(f"we are asking: {query}")
-            result = self._searchable_pdf.query(query)
+            # Default to empty string if not provided
+            query = request.args.get('query', '')
+
+            print(f'we are asking: {query}')
+            result = context.searchable_pdf.query(query)
             return jsonify(result)
 
         @self._server.route('/shutdown')
