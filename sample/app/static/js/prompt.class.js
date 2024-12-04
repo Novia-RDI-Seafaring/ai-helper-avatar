84286298d35dc39461a0e226929a734ae523be0c
ashish-dahal <ashishdahal1997@gmail.com>
2024-12-04 13:40:57 +0200

    Fix error from wrong contant name

--- sample/app/static/js/prompt.class.js
+++ sample/app/static/js/prompt.class.js
@@ -35,11 +35,11 @@ export default class Prompt {
                 })
                 .then(data => {
                     // Hard-coded for debuging.. fix this later!
-                    const imageWidth = 1045 // 1684
-                    const imageHeight = 784 // 1191
+                    const WIDTH = 1045 // 1684
+                    const HEIGHT = 784 // 1191
 
                     this.#displayMessageHistory(data.message_history);
-                    const {status, focus_point, degrees, bboxes} = data
+                    const { status, focus_point, degrees, bboxes } = data
                     console.log('original fp:', focus_point)
                     // points to upper right corner of bbox
                     // focus_point[0] = bboxes[0][0] / WIDTH 
@@ -53,8 +53,8 @@ export default class Prompt {
                     console.log("this is the bbox", bboxes)
                     console.log("this is the first elemnt of bbox", bboxes[0][0])
                     console.log("This the final focus point", focus_point[0], focus_point[1])
-                    
-                    context.avatar.handleMessage(context, {status, focus_point, direction: degrees});
+
+                    context.avatar.handleMessage(context, { status, focus_point, direction: degrees });
                     context.whiteboard.loadPdf(context, bboxes)
                 })
                 .catch(e => {
@@ -81,7 +81,7 @@ export default class Prompt {
         chatInputForm.addEventListener('submit', event => {
             event.preventDefault();
             sendForm();
-        }); 
+        });
     }
 
     #displayMessage(message, type) {
@@ -101,16 +101,16 @@ export default class Prompt {
     #displayMessageHistory(messageHistory) {
         const chatUI = document.querySelector('.chat-ui');
         const chat = document.querySelector('.chat');
-    
+
         if (messageHistory.length === 0) return; // Exit if there's no history
-    
+
         // Get the last message from the history
         const lastMessage = messageHistory[messageHistory.length - 1];
         const type = lastMessage.is_user ? 'outgoing' : 'incoming';
-    
+
         const messageElement = document.createElement('div');
         messageElement.className = 'message ' + type;
-    
+
         // Check if the message is intended to be displayed as a list
         // const isList = lastMessage.message.toLowerCase().includes('list of items:');
         const isList = lastMessage.message.split('\n').some(line => line.trim().startsWith('-'));
@@ -120,9 +120,9 @@ export default class Prompt {
             messageElement.innerHTML = `<p class="messageText">${lastMessage.message}</p><pre class="messageTime">${new Date().toLocaleTimeString()}</pre>`;
         }
         chat.appendChild(messageElement);
-    
+
         // Scroll to the bottom of the chat window
         chatUI.scrollTop = chatUI.scrollHeight;
     }
-    
+
 }
